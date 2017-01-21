473fdfebd12abcec792ef651ec7230d341261d24
Luke Frizzell <lukefrizzell97@gmail.com>
2017-01-21 17:33:22 +0000

    floating sphere to be planet

--- Projectile/javascript/main.js
+++ Projectile/javascript/main.js
@@ -1,1 +1,12 @@
 var blipp = require('blippar').blipp;
+
+blipp.read("main.json");
+
+var scene = blipp.addScene("default");
+
+scene.onCreate = function() {
+  var earth = scene.addMesh("cube1.md2");
+  var dimension = blipp.getScreenWidth()/2;
+  earth.addScale(100);
+  earth.setTranslation(0,0,500);
+}

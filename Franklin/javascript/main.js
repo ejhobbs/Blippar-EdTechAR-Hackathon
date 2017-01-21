8bb8989caeec789a21955d4fc855069e92d47857
Edward Hobbs <eh362@kent.ac.uk>
2017-01-21 16:49:47 +0000

    syntax errors

--- Franklin/javascript/main.js
+++ Franklin/javascript/main.js
@@ -1,1 +1,16 @@
 var blipp = require('blippar').blipp;
+
+blipp.read("main.json");
+
+var franklinScene = blipp.addScene();
+
+var markerWidth = blipp.getMarker().getWidth();
+var markerHeight = blipp.getMarker().getHeight();
+
+franklinScene.onCreate = function(){
+  console.log("scene created");
+}
+
+franklinScene.onShow = function(){
+  console.log("Scene displayed");
+}

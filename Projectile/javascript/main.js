686aba148742a2d1d8f26845fb205c92e9e791dc
Luke Frizzell <lukefrizzell97@gmail.com>
2017-01-21 18:11:56 +0000

    added cannon and slight refactor

--- Projectile/javascript/main.js
+++ Projectile/javascript/main.js
@@ -5,8 +5,13 @@ blipp.read("main.json");
 var scene = blipp.addScene("default");
 
 scene.onCreate = function() {
-  var earth = scene.addMesh("cube1.md2");
-  var dimension = blipp.getScreenWidth()/2;
-  earth.addScale(100);
+
+  var earth = scene.addMesh("Earth.md2");
+  earth.setScale(200);
   earth.setTranslation(0,0,500);
+
+  var cannon = scene.addMesh("Cannon.md2");
+  cannon.setScale(20);
+  cannon.setTranslation(0,280,500);
+  cannon.addRotation(-90,0,0);
 }

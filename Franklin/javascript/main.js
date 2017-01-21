79d27fcf27d96888c1ed0fe612c406619f12aa3f
Edward Hobbs <eh362@kent.ac.uk>
2017-01-21 17:18:31 +0000

    get screen dimensions

--- Franklin/javascript/main.js
+++ Franklin/javascript/main.js
@@ -7,6 +7,9 @@ var franklinScene = blipp.addScene();
 var markerWidth = blipp.getMarker().getWidth();
 var markerHeight = blipp.getMarker().getHeight();
 
+var screenHeight = blipp.getScreenHeight() * 1.003;
+var screenWidth = blipp.getScreenWidth() * 1.003;
+
 franklinScene.onCreate = function(){
   console.log("scene created");
 }

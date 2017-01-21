43ef979258068a70c8f5c7b318d0905fe76d5580
Unknown <lukefrizzell97@gmail.com>
2017-01-21 17:10:42 +0000

    project start

--- Projectile/javascript/main.js
+++ Projectile/javascript/main.js
@@ -1,1 +1,14 @@
 var blipp = require('blippar').blipp;
+
+blipp.read("main.json");
+
+var scene = blipp.addScene("default");
+
+scene.onCreate = function() {
+  var earth = scene.addSprite();
+  earth.addMesh("cube1.md2");
+  earth.addMesh("cube2.md2");
+  var dimension = scene.getWidth()/2;
+  earth.setHeight(dimension):
+  earth.setWidth(dimension);
+}

b3b49cab60422c54cd864db73435520ece6e670f
Luke Frizzell <lukefrizzell97@gmail.com>
2017-01-22 12:47:52 +0000

    Added text for too fast sattelite.

--- Projectile/javascript/main.js
+++ Projectile/javascript/main.js
@@ -6,13 +6,17 @@ var sattelite = node.addMesh("Cannon.md2");
 var earth = scene.addMesh("Earth.md2");
 
 var screen = scene.getScreen();
-var orbit = screen.addSprite();
-var overpower = screen.addSprite();
-var crash = screen.addSprite();
+var orbit = screen.addSprite("JUST ENOUGH.png");
+var overpower = screen.addSprite("TOO FAST.png");
+var crash = screen.addSprite("TOO SLOW.png");
 
 var textback = screen.addSprite();
 
 var rotation = null;
+
+scene.onCreate = function() {
+  blipp.hideUiComponents('navBar');
+}
 
 scene.onShow = function() {
 
@@ -25,22 +29,20 @@ scene.onShow = function() {
   sattelite.addRotation(-90,0,0);
   sattelite.setColor("#FF0000");
 
-  orbit.setColor("#0000FF");
-  orbit.setHeight(blipp.getScreenHeight()/2.5);
-  orbit.setWidth(blipp.getScreenWidth()/2);
-  orbit.setTranslation([0, -(blipp.getScreenHeight()/2), 0]);
+  orbit.setHeight(blipp.getScreenWidth()/2.5);
+  orbit.setWidth(blipp.getScreenWidth()/2.5);
+  orbit.setTranslation([0, -(blipp.getScreenHeight()/2)+200, 0]);
 
-  overpower.setColor("#FF0000");
-  overpower.setHeight(blipp.getScreenHeight()/2.5);
-  overpower.setWidth(blipp.getScreenWidth()/2);
-  overpower.setTranslation([(blipp.getScreenWidth()/2), -(blipp.getScreenHeight()/2), 0]);
+  overpower.setHeight(blipp.getScreenWidth()/2.5);
+  overpower.setWidth(blipp.getScreenWidth()/2.5);
+  overpower.setTranslation([(blipp.getScreenWidth()/2)-150, -(blipp.getScreenHeight()/2)+200, 0]);
 
-  crash.setColor("#FF0000");
-  crash.setHeight(blipp.getScreenHeight()/2.5);
-  crash.setWidth(blipp.getScreenWidth()/2);
-  crash.setTranslation([-(blipp.getScreenWidth()/2), -(blipp.getScreenHeight()/2), 0]);
+  crash.setHeight(blipp.getScreenWidth()/2.5);
+  crash.setWidth(blipp.getScreenWidth()/2.5);
+  crash.setTranslation([-(blipp.getScreenWidth()/2)+150, -(blipp.getScreenHeight()/2)+200, 0]);
 
-  textback.setColor("#000000");
+  textback.setColor("#FFFFFF");
+  textback.setAlpha(0.9);
   textback.setHeight(blipp.getScreenHeight());
   textback.setWidth(blipp.getScreenWidth());
   textback.setHidden(true);
@@ -72,5 +74,6 @@ overpower.on('touchEnd', function() {
   rotation.stop();
   sattelite.animate().translation([-blipp.getScreenHeight(), 0, 0]).duration(2000).on('end', function() {
     textback.setHidden(false);
+    textback.setTexture("fastMessage.png");
   });
 });
